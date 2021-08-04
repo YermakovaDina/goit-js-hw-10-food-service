373af8a2dd8a94fde67d887584d1eba13f578cef
Dina Yermakova <yermakovadina@gmail.com>
2021-08-04 15:18:27 +0300

    added Parcel

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import './sass/main.scss';
+// import menuItemTempl from './templates/menu-template.hbs';
+// import menu from './menu.json';
+
+// const Theme = {
+//   LIGHT: 'light-theme',
+//   DARK: 'dark-theme',
+// };
+
+// const checkbox = document.querySelector('#theme-switch-toggle');
+// const bodyEl = document.querySelector('body');
+// const menuEl = document.querySelector('.js-menu');
+
+// if (localStorage.theme === Theme.DARK) {
+//   checkbox.checked = true;
+//   bodyEl.classList.add(Theme.DARK);
+// }
+
+// checkbox.addEventListener('change', onCheckboxChange);
+// function onCheckboxChange(e) {
+//   if (checkbox.checked === true) {
+//     bodyEl.classList.add(Theme.DARK);
+//     bodyEl.classList.remove(Theme.LIGHT);
+//     localStorage.setItem('theme', Theme.DARK);
+//   } else {
+//     bodyEl.classList.add(Theme.LIGHT);
+//     bodyEl.classList.remove(Theme.DARK);
+//     localStorage.setItem('theme', Theme.LIGHT);
+//   }
+// }
+
+// menuEl.insertAdjacentHTML('beforeend', menuItemTempl(menu));
