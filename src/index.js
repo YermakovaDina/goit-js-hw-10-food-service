aff702fe90c1af90aa83dd8d6524c65def2a1242
Dina Yermakova <yermakovadina@gmail.com>
2021-08-05 17:11:17 +0300

    активация смены темы

--- src/index.js
+++ src/index.js
@@ -2,16 +2,32 @@ import menuCardTpl from './templates/card-template.hbs';
 import menu from './menu.json';
 
 const menuCards = document.querySelector('ul.js-menu');
-const cardsMarkup = createMenuCardTpl(menu);
-menuCards.insertAdjacentHTML('beforeend', cardsMarkup);
-menuRef.innerHTML = menuCardTpl(menu);
-console.log(menuRef);
 
-function createMenuCardTpl(menu) {
-  return menuCardTpl(menu);
-}
+const checkbox = document.querySelector('#theme-switch-toggle');
+const bodyRef = document.querySelector('body');
 
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
+
+menuCards.insertAdjacentHTML('beforeend', menuCardTpl(menu));
+// console.log(menuCards);
+
+if (localStorage.theme === Theme.DARK) {
+  checkbox.checked = true;
+  bodyRef.classList.add(Theme.DARK);
+}
+
+checkbox.addEventListener('change', onChangeAction);
+function onChangeAction(e) {
+  if (checkbox.checked === true) {
+    bodyRef.classList.add(Theme.DARK);
+    bodyRef.classList.remove(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    bodyRef.classList.add(Theme.LIGHT);
+    bodyRef.classList.remove(Theme.DARK);
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+}
