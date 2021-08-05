b5c2e88ede625061c32e9fd215d52e69ebd96f13
Dina Yermakova <yermakovadina@gmail.com>
2021-08-05 15:15:26 +0300

    шаблонизация карточек

--- src/index.js
+++ src/index.js
@@ -1,37 +1,17 @@
-import './styles.css';
+import menuCardTpl from './templates/card-template.hbs';
 import menu from './menu.json';
-import menuTemplatesCard from './templates/card-template.hbs';
 
-menuRef.insertAdjacentHTML('beforeend', menuTemplatesCard(menu));
+const menuCards = document.querySelector('ul.js-menu');
+const cardsMarkup = createMenuCardTpl(menu);
+menuCards.insertAdjacentHTML('beforeend', cardsMarkup);
+menuRef.innerHTML = menuCardTpl(menu);
+console.log(menuRef);
+
+function createMenuCardTpl(menu) {
+  return menuCardTpl(menu);
+}
 
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
-
-// const checkbox = document.querySelector('#theme-switch-toggle');
-// const bodyEl = document.querySelector('body');
-// const menuRef = document.querySelector('.js-menu');
-
-// if (localStorage.theme === Theme.DARK) {
-//   checkbox.checked = true;
-//   bodyEl.classList.add(Theme.DARK);
-// }
-
-// checkbox.addEventListener('change', onCheckboxChange);
-// function onCheckboxChange(e) {
-//   if (checkbox.checked === true) {
-//     bodyEl.classList.add(Theme.DARK);
-//     bodyEl.classList.remove(Theme.LIGHT);
-//     localStorage.setItem('theme', Theme.DARK);
-//   } else {
-//     bodyEl.classList.add(Theme.LIGHT);
-//     bodyEl.classList.remove(Theme.DARK);
-//     localStorage.setItem('theme', Theme.LIGHT);
-//   }
-// }
-
-// === добавление карточек ===
-// const menuRef = document.querySelector('ul.js-menu'); // доступ к списку карточек
-// menuRef.innerHTML = menuTemplatesCard(menu); // добавляем карточки
-// console.log(menuRef);
